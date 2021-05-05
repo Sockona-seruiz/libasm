a1fe0ef95d5929aaa5bf3dda501850e1aab5fd38
Sébastien Ruiz <s.ruiz.sin@gmail.com>
2021-05-05 17:49:44 +0200

    further clean

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -1,39 +1,21 @@
 		global 	_ft_strcmp
 		section .text
 _ft_strcmp:
-	xor		rax, rax		;rdi arg[0] rsi arg[1]
-	xor		r8, r8
-	dec		r8
+	xor		rax, rax
+	xor		rcx, rcx
 	jmp		while
 
-notequal:
-	jl		retneg
-	jnl		retpos
-	jmp		retzero
-
 while:
-	inc		r8
-	mov		al , BYTE[rdi + r8]
-	mov		cl , BYTE[rsi + r8]
-	cmp		al, cl
-	jne		notequal
+	mov		al , [rdi]
+	mov		cl , [rsi]
 	cmp		al, 0
-	je		retzero
-	cmp		cl, 0
-	je		retzero
+	je		done
 	cmp		al, cl
-	je		while
+	jne		done
+	inc 	rdi
+	inc 	rsi
+	jmp		while
 
-	jmp		notequal
-
-retpos:
-	mov		rax, 1
-	ret
-
-retneg:
-	mov		rax, -1
-	ret
-
-retzero:
-	mov		rax, 0
+done:
+	sub		rax, rcx
 	ret
